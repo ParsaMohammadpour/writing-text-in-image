1dc421726939ad2ab2e98d0210a937a8341d14d2
Parsa Mohammadpour <parsa1380p.m.p@gmail.com>
2022-11-03 21:48:45 +0330

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from PIL import Image, ImageDraw, ImageFont
+
+image = Image.open('image.jpg')
+image.show()
+
+draw = ImageDraw.Draw(image)
+text = 'Parsa Mohammadpour - 98243050'
+
+font = ImageFont.truetype('arial.ttf', 40)
+
+width, height = image.width/2, image.height/2
+
+draw.text((width, height), text, font=font, anchor="mm", fill=(255,0,0))
+
+image.show()
+image.save('Result.jpg')
